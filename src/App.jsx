d63ba9fc8955b2bdc66204f6344aa42068bfa0e3
MahmoudAlla88 <hamoodaalla88@gmail.com>
2025-02-11 10:23:33 +0300

    Auth

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,30 @@ import Footer from "./components/footer"
 import Home from "./components/Home/Home"
 import Aboutus from "./components/AboutUs/Aboutus"
 import Contactus from "./components/ContactUs/Contactus"
+// import SignUp from "./components/SignUp/SignUp"
+import LoginPage from "./components/LogIn/Login"
+import SignUp from "./components/SignUp/SignUp"
 
 
 
 function App() {
 
   return (
+      // <Router>
+      //   <Navbar />
+      //   <Routes>
+      //     <Route path="/" element = {<Home />}></Route>
+      //     <Route path="/aboutUs" element = {<Aboutus />}></Route>
+      //     <Route path="/contactUs" element = {<Contactus />}></Route>
+      //   </Routes>
+      //   <Footer />
+      // </Router>
+      // <LoginPage/>
       <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element = {<Home />}></Route>
-          <Route path="/aboutUs" element = {<Aboutus />}></Route>
-          <Route path="/contactUs" element = {<Contactus />}></Route>
-        </Routes>
-        <Footer />
-      </Router>
+      <Routes>
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes></Router>
   )
 }
 
